1826bb483fcb85ddece87ff93bccfecd782c07b6
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2026-03-10 10:33:04 +0900

    Add new PCI IDs for Renoir and related chips

--- drivers/gpu/drm/amd/amdgpu/amdgpu_drv.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_drv.c
@@ -1096,6 +1096,10 @@ static const struct pci_device_id pciidlist[] = {
 	{0x1002, 0x73AF, PCI_ANY_ID, PCI_ANY_ID, 0, 0, CHIP_SIENNA_CICHLID},
 	{0x1002, 0x73BF, PCI_ANY_ID, PCI_ANY_ID, 0, 0, CHIP_SIENNA_CICHLID},
 
+	{0x1002, 0x15E7, PCI_ANY_ID, PCI_ANY_ID, 0, 0, CHIP_RENOIR|AMD_IS_APU}, /* Barcelo / Ryzen 5000U refresh */
+	{0x1002, 0x1681, PCI_ANY_ID, PCI_ANY_ID, 0, 0, CHIP_RENOIR|AMD_IS_APU}, /* Rembrandt 기반 일부 Vega Fallback 지원용 */
+	{0x1002, 0x163F, PCI_ANY_ID, PCI_ANY_ID, 0, 0, CHIP_RENOIR|AMD_IS_APU}, /* Mendocino (일부 모바일/미니PC) */
+	
 	{0, 0, 0}
 };
 
